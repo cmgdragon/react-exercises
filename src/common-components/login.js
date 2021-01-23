6ffde223ca70acb01e20b5787c6addf5dd35d3b2
WáficTheDragon <carlos_ole1996@hotmail.com>
2021-01-23 03:54:27 +0100

    you didn't see this typo

--- src/common-components/login.js
+++ src/common-components/login.js
@@ -6,31 +6,31 @@ import googleIcon from '../img/google.svg';
 const Login = () => {
 
     const signIn = async provider => {
-    
+
         try {
-           const userInfo = await auth.signInWithPopup(provider);
-           console.log(userInfo.additionalUserInfo, userInfo.user.uid)
-           if (userInfo.additionalUserInfo.isNewUser) {
-               registerNewUser(userInfo.user.uid);
-           }
+            const userInfo = await auth.signInWithPopup(provider);
+
+            if (userInfo.additionalUserInfo.isNewUser) {
+                registerNewUser(userInfo.user.uid);
+            }
         } catch (error) {
             console.log(error.message);
         }
-    
+
     }
-    
+
 
     return (
         <div className={'login-wrapper'}>
-        <span className={'dont-worry'}>Please, before start,</span>
-        <div onClick={() => signIn(providers.google)} className={`google-btn provider-btn`} >
-            <div className={`provider-icon-wrapper`}>
-                <img alt="Google login" className={`google-icon provider-icon`} src={googleIcon} />
+            <span className={'dont-worry'}>Please, before start,</span>
+            <div onClick={() => signIn(providers.google)} className={`google-btn provider-btn`} >
+                <div className={`provider-icon-wrapper`}>
+                    <img alt="Google login" className={`google-icon provider-icon`} src={googleIcon} />
+                </div>
+                <p className={'btn-text'}><b>Sign in with Google</b></p>
             </div>
-            <p className={'btn-text'}><b>Sign in with Google</b></p>
+            <span className={'dont-worry'}>(Don't be afraid, he's already watching you anyways)</span>
         </div>
-        <span className={'dont-worry'}>(Don't be afraid, he's already watching you anyways)</span>
-    </div>
     )
 }
 
--- src/common-components/navigation.js
+++ src/common-components/navigation.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { NavLink } from "react-router-dom";
 import { signOut } from '../auth/authProviders';
 
-const Navigation = ({user}) => {
+const Navigation = ({ user }) => {
     return (
         <nav>
             <div className={`sign-out`} onClick={signOut}> Sign Out ({user.displayName})</div>
-            <NavLink to="/bikes" activeClassName='active'>Bikes exercice</NavLink>
-            <NavLink to="/trivia" activeClassName='active'>Trivia exercice</NavLink>
-            <NavLink to="/memory" activeClassName='active'>Memory exercice</NavLink>
+            <NavLink to="/bikes" activeClassName='active'>Bikes exercise</NavLink>
+            <NavLink to="/trivia" activeClassName='active'>Trivia exercise</NavLink>
+            <NavLink to="/memory" activeClassName='active'>Memory exercise</NavLink>
         </nav>
     )
 }
