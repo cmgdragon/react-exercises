162d84e0eab8a6dd4a9aa9e294df4a4901acb007
cmgdragon <carlos_ole1996@hotmail.com>
2021-01-23 02:27:49 +0100

    fixed sort methods

--- src/exercise-memory/components/playersTable.js
+++ src/exercise-memory/components/playersTable.js
@@ -2,22 +2,46 @@ import React from 'react';
 
 const PlayersTable = ({globalMarks, userMarks}) => {
 
-    const sortByBestMark = ({errors: errorsA, spentTime: timeA}, {errors: errorsB, spentTime: timeB}) => {
 
-        if (errorsA < errorsB) {
-            if (timeA < timeB) {
+    const sortByBestMark = ({errors: errorsA, size: sizeA, spentTime: timeA},
+        {errors: errorsB, spentTime: timeB, size: sizeB}) => {
+        
+        if (sizeA < sizeB) {
+            return 1;
+        }
+
+        if (sizeA > sizeB) {
+            return -1;
+        }
+
+        if (sizeA == sizeB) {
+
+            if (errorsA > errorsB) {
+                return 1;
+            }
+    
+            if (errorsA < errorsB) {
                 return -1;
-            } else return 1;
-        }
-        if (errorsA > errorsB) {
-            if (timeA > timeB) {
-                return 1;
-            } else return -1;
-        }
-        if (errorsA == errorsB) {
-            if (timeA > timeB) {
-                return 1;
-            } else return -1;
+            }
+
+            if (errorsA == errorsB) {
+
+                if (timeA > timeB) {
+                    return 1;
+                }
+
+                if (timeA < timeB) {
+                    return -1;
+                }
+        
+                if (timeA == timeB) {
+        
+                        return 0;
+                }
+
+            }
+
+
         }
         
     }
@@ -29,18 +53,22 @@ const PlayersTable = ({globalMarks, userMarks}) => {
         <div className={'global'}>Best users marks: {
             Object.values(globalMarks)
             .sort((a, b) => sortByBestMark(a, b))
-            .map(({nickname, errors, spentTime}, i) => {
+            .splice(0, 3)
+            .map(({nickname, errors, spentTime, size}, i) => {
                 return (
-                    <div className={'personal-mark'} key={nickname+i}><b>{`${nickname} `}</b>{`=> ${errors} errors | ${spentTime}s`}</div>
+                    <div className={'global-mark'} key={nickname+i}>
+                        {i === 0 ? '🥇' : i === 1 ? '🥈' : '🥉'}    
+                    <b>{`${nickname}`}</b>{`${size} emojis | ${errors} errors | ${spentTime}s`}</div>
                 )
             })
         }</div>
         <div className={'personal'}>Best personal marks: {
             Object.values(userMarks)
             .sort((a, b) => sortByBestMark(a, b))
-            .map(({nickname, errors, spentTime}, i) => {
+            .splice(0, 5)
+            .map(({nickname, errors, spentTime, size}, i) => {
                 return (
-                    <div className={'global-mark'} key={nickname+i}>{`${errors} errors | ${spentTime}s`}</div>
+                    <div className={'personal-mark'} key={nickname+i}>{`${size} emojis | ${errors} errors | ${spentTime}s`}</div>
                 )
             })
         }</div>
--- src/exercise-trivia/components/playersTable.js
+++ src/exercise-trivia/components/playersTable.js
@@ -5,46 +5,58 @@ const PlayersTable = ({globalMarks, userMarks}) => {
     const sortByBestMark = ({points: pointsA, spentTime: timeA}, {points: pointsB, spentTime: timeB}) => {
 
         if (pointsA < pointsB) {
+            return 1;
+        }
+
+        if (pointsA > pointsB) {
+            return -1;
+        }
+
+        if (pointsA == pointsB) {
+
+            if (timeA > timeB) {
+                return 1;
+            }
+    
             if (timeA < timeB) {
                 return -1;
-            } else return 1;
-        }
-        if (pointsA > pointsB) {
-            if (timeA > timeB) {
-                return 1;
-            } else return -1;
-        }
-        if (pointsA == pointsB) {
-            if (timeA > timeB) {
-                return 1;
-            } else return -1;
+            }
+
+            if (timeA == timeB) {
+
+                return 0;
+
+            }
+
         }
         
     }
     
     return (
         <>
-            { globalMarks !== 'loading' && userMarks !== 'loading' ?
-        <div className={'players-table'}>
-        <div className={'global'}>Best users marks: {
-            Object.values(globalMarks)
-            .sort((a, b) => sortByBestMark(a, b))
-            .map(({nickname, points, spentTime}, i) => {
-                return (
-                    <div className={'personal-mark'} key={nickname+i}><b>{`${nickname} `}</b>{`=> ${points}p | ${spentTime}s`}</div>
-                )
-            })
-        }</div>
-        <div className={'personal'}>Best personal marks: {
-            Object.values(userMarks)
-            .sort((a, b) => sortByBestMark(a, b))
-            .map(({nickname, points, spentTime}, i) => {
-                return (
-                    <div className={'global-mark'} key={nickname+i}>{`${points}p | ${spentTime}s`}</div>
-                )
-            })
-        }</div>
-        </div>
+        { globalMarks !== 'loading' && userMarks !== 'loading' ?
+            <div className={'players-table'}>
+            <div className={'global'}>Best users marks: {
+                Object.values(globalMarks)
+                .sort((a, b) => sortByBestMark(a, b)).splice(0, 3)
+                .map(({nickname, points, spentTime}, i) => {
+                    return (
+                        <div className={'global-mark'} key={nickname+i}>
+                            {i === 0 ? '🥇' : i === 1 ? '🥈' : '🥉'}   
+                        <b>{`${nickname} `}</b>{`${points}p | ${spentTime}s`}</div>
+                    )
+                })
+            }</div>
+            <div className={'personal'}>Best personal marks: {
+                Object.values(userMarks)
+                .sort((a, b) => sortByBestMark(a, b)).splice(0, 5)
+                .map(({nickname, points, spentTime}, i) => {
+                    return (
+                        <div className={'personal-mark'} key={nickname+i}>{`${points}p | ${spentTime}s`}</div>
+                    )
+                })
+            }</div>
+            </div>
         : 'loading'}
 
         <button onClick={() => window.location.reload()}>Restart</button>
