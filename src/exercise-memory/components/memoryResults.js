9c97506dd03dcd8a6e15079ec38994304ce0fde7
cmgdragon <carlos_ole1996@hotmail.com>
2021-01-23 02:04:13 +0100

    adjusting memory game results

--- src/exercise-memory/components/memoryResults.js
+++ src/exercise-memory/components/memoryResults.js
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from 'react';
 import PlayersTable from './playersTable';
 import * as memoryService from '../services/memoryService';
 
-const MemoryResults = ({user, errors, nickname, spentTime}) => {
+const MemoryResults = ({user, errors, nickname, spentTime, emojisNumber}) => {
 
     const [userMarks, updateUserMarks] = useState('loading');
     const [globalMarks, updateGlobalMarks] = useState('loading');
 
     useEffect(() => {
         Promise.all([
-            memoryService.addNewGlobalMark({nickname: nickname, errors: errors, spentTime: spentTime}),
-            memoryService.addNewUserMark(user.uid, {nickname: nickname, errors: errors, spentTime: spentTime}),
+            memoryService.addNewGlobalMark({nickname: nickname, errors: errors, spentTime: spentTime, size: emojisNumber}),
+            memoryService.addNewUserMark(user.uid, {nickname: nickname, errors: errors, spentTime: spentTime, size: emojisNumber}),
         ]).then(() => console.log('sent'));
         memoryService.getGlobalMarks().then(marks => updateGlobalMarks(marks));
         memoryService.getUserMarks(user.uid).then(marks => updateUserMarks(marks));
@@ -18,9 +18,10 @@ const MemoryResults = ({user, errors, nickname, spentTime}) => {
 
     return (
         <>
-        <div className={'errors'}>Errors: {errors}</div>
-        <div className={'time'}>Elapsed time: {spentTime}s</div>
-        <PlayersTable userMarks={userMarks} globalMarks={globalMarks} />
+        <div className={'results'}>Errors: {errors}</div>
+        <div className={'results'}>Elapsed time: {spentTime}s</div>
+        <div className={'results'}>Complexity: {emojisNumber} emojis</div>
+        <PlayersTable userMarks={userMarks} globalMarks={globalMarks} emojisNumber={emojisNumber} />
         </>
     )
 }
